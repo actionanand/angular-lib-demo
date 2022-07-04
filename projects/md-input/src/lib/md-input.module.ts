3ac026ab6dc83bd6e78dd892945a9628af385118
Anand raja <meetanandns@gmail.com>
2022-07-04 15:46:16 +0000

    material input added

--- projects/md-input/src/lib/md-input.module.ts
+++ projects/md-input/src/lib/md-input.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+
 import { MdInputComponent } from './md-input/md-input.component';
+import { InputRefDirective } from './directives/input-ref.directive';
 
 
 
 @NgModule({
   declarations: [
-    MdInputComponent
+    MdInputComponent,
+    InputRefDirective
   ],
   imports: [
   ],
   exports: [
-    MdInputComponent
+    MdInputComponent,
+    InputRefDirective
   ]
 })
 export class MdInputModule { }
--- projects/md-input/src/public-api.ts
+++ projects/md-input/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of md-input
  */
 
-export * from './lib/md-input.service';
-export * from './lib/md-input/md-input.component';
 export * from './lib/md-input.module';
+export * from './lib/md-input/md-input.component';
+export * from './lib/directives/input-ref.directive';
