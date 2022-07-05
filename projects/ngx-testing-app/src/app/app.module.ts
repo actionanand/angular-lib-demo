c297aaefdebec9868ad359e4adfba61cd90e013a
Anand raja <meetanandns@gmail.com>
2022-07-05 17:44:24 +0000

    read me update

--- projects/ngx-testing-app/src/app/app.module.ts
+++ projects/ngx-testing-app/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 // you can import from npm repo also, if you had it published to npm
 import { MdInputModule } from 'projects/md-input/src/public-api';
+import { SvgIconModule } from 'projects/svg-icon/src/public-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,8 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    MdInputModule
+    MdInputModule,
+    SvgIconModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
